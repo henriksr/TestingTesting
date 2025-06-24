13f2dae6078bfa8f92a81d6233834cd53c190d96
Henrik Rosqvist <drummeln@gmail.com>
2025-06-24 15:48:01 +0200

    testapp.tf with first content

---
+++ test-write-zero-trust-apps/testapp.tf
@@ -0,0 +1,19 @@
+module "app-name-use-name-with-dash-for-whitespace" {
+  source   = "./modules/app"
+  app_name = "app name free formatting"
+  destinations = [
+    {
+      type       = "private"
+      uri        = "IP.addr:3389"
+      cidr       = "IP.addr"
+      port_range = "3389"
+    },
+    {
+      type       = "private"
+      uri        = "IP.addr:1433"
+      cidr       = "IP.addr"
+      port_range = "1433"
+    },
+  ]
+  policies = [var.POC_IT, var.POC_Common]
+}
